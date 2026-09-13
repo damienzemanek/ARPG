ca09ffb6fd7c7f3595e0e05a559e8ca177e5eb4a
damien <damienzemanek@lcad.edu>
2026-09-12 17:36:07 -0700

    +

--- Assets/Gamefiles/Scripts/Battlemode/BattlerDisplayGenerator.cs
+++ Assets/Gamefiles/Scripts/Battlemode/BattlerDisplayGenerator.cs
@@ -15,30 +15,37 @@ public class BattlerDisplayGenerator : MonoBehaviour
     public Color healColor;
     public Color damageColor;
     public Color armorColor;
+    public Color buffColor => Color.limeGreen;
+    public Color debuffColor => Color.mediumPurple;
     
     [Button]
     public void GenerateNumberDisplay(int amount, bool crit, ActionIdentifier actionIdentifier)
     {
-        var display = Instantiate(dmgNumberDisplayPrefab, transform);
-        display.transform.position = spawnpos.position;
-        var text = display.GetComponentInChildren<TextMeshPro>();
         var color = actionIdentifier switch
         {
             ActionIdentifier.Attack 
-            or ActionIdentifier.AttackBuff
-            or ActionIdentifier.AttackDebuff 
-            or ActionIdentifier.AttackMove => damageColor,
+                or ActionIdentifier.AttackBuff
+                or ActionIdentifier.AttackDebuff 
+                or ActionIdentifier.AttackMove => damageColor,
             ActionIdentifier.Heal 
-            or ActionIdentifier.HealBuff 
-            or ActionIdentifier.HealDebuff 
-            or ActionIdentifier.HealMove => healColor,
+                or ActionIdentifier.HealBuff 
+                or ActionIdentifier.HealDebuff 
+                or ActionIdentifier.HealMove => healColor,
             ActionIdentifier.Armor
-            or ActionIdentifier.ArmorBuff
-            or ActionIdentifier.ArmorDebuff 
-            or ActionIdentifier.ArmorMove => armorColor,
-            _ => damageColor
+                or ActionIdentifier.ArmorBuff
+                or ActionIdentifier.ArmorDebuff 
+                or ActionIdentifier.ArmorMove => armorColor,
+            ActionIdentifier.Buff
+                or ActionIdentifier.BuffMove => buffColor,
+            ActionIdentifier.Debuff
+                or ActionIdentifier.DebuffMove => debuffColor
         };
+        if (color == buffColor || color == debuffColor) return;
+        var display = Instantiate(dmgNumberDisplayPrefab, transform);
+        display.transform.position = spawnpos.position;
+        var text = display.GetComponentInChildren<TextMeshPro>();
+        string amountText = amount.ToString();
         text.color = crit ? critColor : color;
-        text.text  = crit ? critPretext + amount.ToString() : amount.ToString();
+        text.text  = crit ? critPretext + amountText : amountText;
     }
 }
